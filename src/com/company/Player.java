e7a759f2314d2e034c7901df91c645bed4700d40
gniht <jasonthomasjat@gmail.com>
2018-07-27 13:05:19 -0700

    Saving state just to avoid possible loss of work.

---
+++ src/com/company/Player.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Player {
+  private String name;
+  private int score;
+
+
+  public Player(String name){
+    this.name = name;
+    this.score = 0;
+  }
+
+
+}
